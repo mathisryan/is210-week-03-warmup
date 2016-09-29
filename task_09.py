98bb2c99b2bf08f375b97d60e2680124ffff87ad
Michael Ryan <michael.ryan@spsmail.cuny.edu>
2016-09-29 10:13:05 +0000

    Completed Task 09 Week 03 Warmup

--- task_09.py
+++ task_09.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""A computer that can build the question."""
+"""A computer that can build the question is good but...
+this is my docstring for Task 09
+Week 03 Warmup
+"""
+
 
 THE_ANSWER_TO_EVERYTHING = 42
 
