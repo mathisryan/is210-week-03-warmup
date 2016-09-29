7da15d7202950419eec71864d1229522fddfbf16
Michael Ryan <michael.ryan@spsmail.cuny.edu>
2016-09-29 11:49:00 +0000

    Completed Task 12 Week 03 Warmup

---
+++ task_12.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding:  utf-8 -*-
+"""Task 12 Week 03 Warmup - Numeric Types."""
+
+from decimal import Decimal
+
+from fractions import Fraction
+
+INTVAL = '1'
+
+FLOATVAL = '0.1'
+
+DECVAL = Decimal('0.1')
+
+FRACVAL = Fraction(1, 10)
