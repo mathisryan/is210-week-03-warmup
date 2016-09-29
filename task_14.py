592dd7a8e9348650f58e680d278d8dc476f8d42f
Michael Ryan <michael.ryan@spsmail.cuny.edu>
2016-09-29 12:03:53 +0000

    Completed Task 14 Week 03 Warmup

---
+++ task_14.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding:  utf-8 -*-
+"""Task 14 Week 03 Warmup - Integer Equivalence."""
+
+IS_TRUE = True
+
+IS_FALSE = False
+
+IS_NONE = None
+
+INTEGER_EQUIV = IS_TRUE == 1 and IS_FALSE == 0
